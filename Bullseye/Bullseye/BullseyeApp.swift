6c84e2f03d0453c28dc3e2b8f9051bb8dbfed802
Jonathan Vineyard <jonathanvineyard@Jonathans-MacBook-Pro.local>
2025-09-15 09:50:45 -0600

    Created project

---
+++ Bullseye/Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jonathan Vineyard on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/Bullseye/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Jonathan Vineyard on 9/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
